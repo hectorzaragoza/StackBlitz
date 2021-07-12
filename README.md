e728832fe3712e86e2bc421904286ab2b9078070
hectorzaragoza <zaragoza09@gmail.com>
2021-07-12 14:52:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-z5z9kb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-z5z9kb)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let firstName = 'Hector'
+console.log(firstName)
+
+let lastName = 'Zaragoza'
+console.log(lastName)
+
+console.log(firstName + ' ' + lastName)
